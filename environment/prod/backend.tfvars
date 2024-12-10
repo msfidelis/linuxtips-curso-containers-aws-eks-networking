9f65c66014dc19f9184f0469caf37d9a751c1aad
Matheus Fidelis <matheus@nanoshots.com.br>
2024-12-09 21:14:14 -0300

    Aula VPC

---
+++ environment/prod/backend.tfvars
@@ -0,0 +1,3 @@
+bucket = "linuxtips-s3-eks-state-files"
+key    = "eks/vpc/prod/state"
+region = "us-east-1"
---
+++ variables.tf
@@ -0,0 +1,46 @@
+variable "project_name" {
+  type = string
+}
+
+variable "region" {
+  type = string
+}
+
+variable "vpc_cidr" {
+  type    = string
+  default = "CIDR principal da VPC"
+}
+
+variable "vpc_additional_cidrs" {
+  type        = list(string)
+  description = "Lista de CIDR's adicionais da VPC"
+  default     = []
+}
+
+variable "public_subnets" {
+  description = "Lista de Public Subnets da VPC"
+  type = list(object({
+    name              = string
+    cidr              = string
+    availability_zone = string
+  }))
+}
+
+variable "private_subnets" {
+  description = "Lista de Private Subnets da VPC"
+  type = list(object({
+    name              = string
+    cidr              = string
+    availability_zone = string
+  }))
+}
+
+variable "database_subnets" {
+  description = "Lista de Database Subnets da VPC"
+  default     = []
+  type = list(object({
+    name              = string
+    cidr              = string
+    availability_zone = string
+  }))
+}
